9b61bfaef1507240bbc9d75ac97dcadd0dff7731
KunySrc <101210196+KunySrc@users.noreply.github.com>
2022-06-15 17:10:11 +0200

    newq

--- animatedCanvas.js
+++ animatedCanvas.js
@@ -1,22 +1,45 @@
-var canvas = document.getElementById("animatedCanvas");
-var ctx=canvas.getContext("2d");
-//ctx.font="30px Comic Sans MS";
-//ctx.fillStyle = "red";
-//ctx.textAlign = "center";
-//ctx.fillText("Hello World", canvas.width/2, canvas.height/2);
-//ctx.drawImage("animatedCanvasImg.jpeg", 0, 0, 0, 0);
+window.onload=(function(){
 
+    // canvas related variables
+    var canvas=document.getElementById("animatedCanvas");
+    var ctx=canvas.getContext("2d");
+    var cw=canvas.width;
+    var ch=canvas.height;
 
-var img1 = new Image();
+    // animation related variables
+    var minX=0;        // Keep the image animating 
+    var maxX=120;       // between minX & maxX
+    var x=minX;         // The current X-coordinate
+    var speedX=1;       // The image will move at 1px per loop 
+    var direction=1;    // The image direction: 1==righward, -1==leftward
+    var y=-380;           // The Y-coordinate
 
-        //drawing of the test image - img1
-        img1.onload = function () {
-            //draw background image
-            ctx.drawImage(img1, 0, 0);
-            //draw a box over the top
-            ctx.fillStyle = "rgba(200, 0, 0, 0.5)";
-            ctx.fillRect(0, 0, 500, 500);
+    // Load a new image
+    // IMPORTANT!!! You must give the image time to load by using img.onload!
+    var img=new Image();
+    img.onload=start;
+    img.src="bild.png";
+    function start(){
+        // the image is fully loaded sostart animating
+        requestAnimationFrame(animate);
+    }
+    ctx.scale(0.2, 1);
+    function animate(time){
 
-        };
+        // clear the canvas
+        ctx.clearRect(0,0,cw,ch);
+        //ctx.scale(2, 2);
+        // draw
+        ctx.drawImage(img,x,y);
 
-        img1.src = 'animatedCanvasImg.jpeg';
+        // update
+        x += speedX * direction;
+        // keep "x" inside min & max
+        if(x<minX){ x=minX; direction*=-1; }
+        if(x>maxX){ x=maxX; direction*=-1; }
+
+        // request another loop of animation
+        requestAnimationFrame(animate);
+    }
+
+}); // end $(function(){});
